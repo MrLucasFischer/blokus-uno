49dbb74d1c7a1fa7fbd72b51afb27ae3c2a52c01
Lucas Fischer <lucasfischerpt@gmail.com>
2017-11-30 15:01:45 +0000

    implementacao do menu principal

--- projeto.lisp
+++ projeto.lisp
@@ -1,6 +1,6 @@
 ;;;;Disciplina de IA - 2017/2018
 ;;;;Projeto 1 - Blokus Uno
-;;;;Autor: Andreia Pereira (nº 150221021) e Lucas Fischer (nº 140221004)
+;;;;Autor: Andreia Pereira (nï¿½ 150221021) e Lucas Fischer (nï¿½ 140221004)
 
 ;;;;Funcoes de interacao com o utilizador e de escrita e leitura de ficheiros
 
@@ -13,8 +13,11 @@
        )
     ;Para Ubuntu, no meu caso:
     ;/home/lucas-fischer/Dropbox/Faculdade/IA/2018/Projeto/blokus-uno
-    (compile-file (concatenate 'string caminho "/puzzle.lisp"))
-    (compile-file (concatenate 'string caminho "/procura.lisp"))
+    (progn
+      (compile-file (concatenate 'string caminho "/puzzle.lisp"))
+      (compile-file (concatenate 'string caminho "/procura.lisp"))
+      (menu-principal caminho)
+    )
   )
 )
 
@@ -23,8 +26,72 @@
 ;; ler-caminho
 
 (defun ler-caminho (&optional (caminho nil))
+  "Funcao que questiona o utilizador pelo caminho onde se encontra os ficheiros do projeto, devolvendo a sua resposta"
   (cond
-   ((null caminho) (format T "Introduza o caminho até ao diretorio do projeto~%") (ler-caminho (read-line)))
+   ((null caminho) (format T "Introduza o caminho atï¿½ ao diretorio do projeto~%") (ler-caminho (read-line)))
    (T caminho)
+  )
+)
+
+
+
+
+;; menu-principal
+
+(defun menu-principal (caminho)
+  "Funcao que ira mostrar ao utilizador o menu principal da aplicacao blokus-uno dando-lhe a escolher qual a acao desejada"
+
+  ;;Ao Correr ficara com a formatacao correta
+  (progn
+    (format T "~% |---------------------------------------------------|")
+    (format T "~% |                  ______                 _____     |")
+    (format T "~% |    |---   |     /      \\ | /  |    |   |     \\    |")
+    (format T "~% |    |   |  |     |      | |/   |    |   \\_____     |")
+    (format T "~% |    |---   |     |      | |\\   |    |         \\    |")
+    (format T "~% |    |   |  |     |      | | \\  |    |   \\     |    |")
+    (format T "~% |    |---   |____  \\____/  |  \\  \\__/     \\___/     |")
+    (format T "~% |                               ____                |")
+    (format T "~% |               |     | |\\   | /    \\               |")
+    (format T "~% |               |     | | \\  | |    |               |")
+    (format T "~% |               |     | |  \\ | |    |               |")
+    (format T "~% |                \\___/  |   \\|  \\___/               |")
+    (format T "~% |                                                   |")
+    (format T "~% |                                                   |")
+    (format T "~% |                   1 - Iniciar                     |")
+    (format T "~% |                   2 - Sair                        |")
+    (format T "~% |                                                   |")
+    (format T "~% |---------------------------------------------------|")
+
+    (let* (
+            
+          )
+
+    )
+  )
+)
+
+(defun menu-principal-2 (caminho)
+  "Funcao que ira mostrar ao utilizador o menu principal da aplicacao blokus-uno dando-lhe a escolher qual a acao desejada"
+  (progn
+    (format T "~% |---------------------------------------------------|")
+    (format T "~% |                                                   |")
+    (format T "~% |                       B                           |")
+    (format T "~% |                        L                          |")
+    (format T "~% |                         O                         |")
+    (format T "~% |                          K                        |")
+    (format T "~% |                        U  U                       |")
+    (format T "~% |                         N  S                      |")
+    (format T "~% |                          O                        |")
+    (format T "~% |                                                   |")
+    (format T "~% |                   1 - Iniciar                     |")
+    (format T "~% |                   2 - Sair                        |")
+    (format T "~% |                                                   |")
+    (format T "~% |---------------------------------------------------|")
+
+    (let* (
+            
+          )
+
+    )
   )
 )
